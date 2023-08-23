3275e56cb60ded93deba2b7f3d2ba03b955f8e9c
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-08-23 07:54:35 -0600

    chain lear

---
+++ chain.js
@@ -0,0 +1,13 @@
+const user = [{
+    name: 'Jalil khan',
+    home:{
+        country: 'BanglaDesh',
+        city: 'Dhaka',
+        address:{
+            street1: 'ratar guli mol',
+            street2: "Porir goli"
+        }
+    }
+}]
+
+console.log(user[0].home.address.street2)
